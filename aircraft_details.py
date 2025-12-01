1133fbf36f7ac62e468043819a4bf6995d361509
b-neveux <bneveux2003@gmail.com>
2025-12-01 16:56:41 +0100

    MAJ sur l'analyse du fichier Aircraft_Data.xlsx

--- aircraft_details.py
+++ aircraft_details.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
-db = pd.read_excel("Sources/data/Aircraft_Data.xlsx")
+df = pd.read_excel("Sources/data/Aircraft_Data.xlsx")
 var_x = "MTOW"
 var_y = "ZFW"
 
-X = db[var_x]
-Y = db[var_y]
+X = df[var_x]
+Y = df[var_y]
+
+sns.pairplot(df.iloc[:,0:9])
 
 plt.scatter(X, Y)
 plt.xlabel(var_x)
